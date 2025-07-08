cee8c75f5194f58eb038ee3b158927134b2d660c
maycol-orosco <1007720221@unajma.edu.pe>
2025-07-07 20:21:09 -0500

    Actualizacion del proyecto

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -8,4 +8,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {}
+export class InicioComponent {
+  modalActivo: boolean = false;
+  esRegistro: boolean = false;
+
+  mostrarLogin() {
+    this.modalActivo = true;
+    this.esRegistro = false;
+  }
+
+  mostrarRegistro() {
+    this.modalActivo = true;
+    this.esRegistro = true;
+  }
+
+  cerrarLogin(event: Event) {
+    event.preventDefault(); // para evitar que recargue la página
+    this.modalActivo = false;
+  }
+}
